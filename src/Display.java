f7f5ecb5355a891394236b31a47bbec789035779
RJ <ravikant642@gmail.com>
2022-09-23 22:34:45 +0530

    User defined method example

---
+++ src/Display.java
@@ -0,0 +1,11 @@
+//Example of static method
+
+public class Display {
+    public static void main(String[] args) {
+        show();
+    }
+
+    static void show() {
+        System.out.println("It is an example of static method.");
+    }
+}

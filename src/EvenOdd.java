647da0bfd66b84cb6833da1f2b43092edfe6741d
RJ <ravikant642@gmail.com>
2022-09-23 07:01:22 +0530

    User defined method example

---
+++ src/EvenOdd.java
@@ -0,0 +1,25 @@
+//User-defined method example
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        //creating Scanner class object
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        //reading value from user
+        int num = scan.nextInt();
+        //method calling
+        findEvenOdd(num);
+    }
+
+    //user defined method
+    private static void findEvenOdd(int num) {
+        //method body
+        if(num%2==0){
+            System.out.println(num+" is even");
+        }else {
+            System.out.println(num+" is odd");
+        }
+    }
+}

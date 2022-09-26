977dfff46e9970c48fd0a1bfb4ec9b9ebd877ea3
RJ <ravikant642@gmail.com>
2022-09-26 23:31:55 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/Student11.java
@@ -0,0 +1,23 @@
+//Example of accessor and mutator method
+
+public class Student11 {
+    private int roll;
+    private String name;
+    public int getRoll(){     //accessor method
+        return roll;
+    }
+    public void setRoll(int roll) {  //mutator method
+
+        this.roll = roll;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void display() {
+        System.out.println("Roll no.: "+roll);
+        System.out.println("Student name: "+name);
+    }
+}

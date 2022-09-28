1c0752a45cfc8c7f373af503ded6b24b01afd068
RJ <ravikant642@gmail.com>
2022-09-29 00:21:11 +0530

    Java Inheritance Example

---
+++ src/Employee.java
@@ -0,0 +1,15 @@
+//Java Inheritance Example
+
+class Employee1 {
+    float salary = 40000;
+}
+
+class Programmer extends Employee1{
+    int bonus = 10000;
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        System.out.println("Programmer salary is : "+p.salary);
+        System.out.println("Programmer bonus is : "+p.bonus);
+    }
+}

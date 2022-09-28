c7ef4ef48f46c0d6235291bd2c974cd5fa6aa543
RJ <ravikant642@gmail.com>
2022-09-28 23:09:06 +0530

    Calling default constructor from parameterized constructor

---
+++ src/TestThis5.java
@@ -0,0 +1,16 @@
+//Calling default constructor from parameterized constructor
+class Aa{
+    Aa(){
+        System.out.println("hello a");
+    }
+    Aa(int x){
+        this();
+        System.out.println(x);
+    }
+}
+
+class TestThis5 {
+    public static void main(String[] args) {
+        Aa a = new Aa(10);
+    }
+}

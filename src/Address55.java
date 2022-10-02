9b60cd70d3e87aa528db8b7769f315a92ce0ee44
RJ <ravikant642@gmail.com>
2022-10-02 07:51:23 +0530

    Address55

---
+++ src/Address55.java
@@ -0,0 +1,9 @@
+public class Address55 {
+    String city, state, country;
+
+    public Address55(String city, String state, String country){
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+}
